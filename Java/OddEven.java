ae51e30cd6de04e109041c709b07eb4be5817398
Vishnu072001 <vk8788957@gmail.com>
2025-10-23 22:23:29 +0530

    a basic java program

---
+++ Java/OddEven.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+// check even or odd 
+public class OddEven{
+    public void oddeven(int n){
+        if(n%2==0)
+            System.out.println("it is an even number");
+        else
+            System.out.println("it is not a even number");
+            
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter a number");
+            int n=sc.nextInt();
+            OddEven bp=new OddEven();
+            bp.oddeven(n);
+
+        }
+
+    }
